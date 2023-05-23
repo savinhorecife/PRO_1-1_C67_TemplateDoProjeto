7a2a45956566044ae20cd70ea4d17a9bc0d935d1
saulo oliveira <savinhorecife@gmail.com>
2023-05-23 14:48:10 -0300

    selton

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-       <Text> savio </Text>
+       <Text> savio e selton </Text>
       </View>
     );
   }
